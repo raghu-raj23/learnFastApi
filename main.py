6ac974ce260f7b95d74e93a957929e382c6fa778
raghu-raj23 <raghuraj.muni@zs.com>
2026-01-15 13:17:08 +0530

    initial code test

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: Optional[float] | None = None
+
+
+@app.get("/items")
+def read_items():
+    return [{"name": "Item Foo"}, {"name": "Item Bar"}]
+
 if __name__ == "__main__":
     print("hello")
